50ad6ff427c4a6573bbde1f986997e4fcbe1623b
AminFitri <aminfitri2000@gmail.com>
2021-09-02 10:31:06 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<?php
+        echo "<h2>The End Of Loneliness</h2>";
+    ?>
+    <?php
+        echo "<h3>In an age when the double tap is so often the fastest path to fame, there is
+        something appealingly old-fashioned about a literary success story. This holds especially true for the
+        tale of Benedict Wells, the 34-year-old German wunderkind who survived a downright Dickensian childhood 
+        before emerging as a cultural sensation in his native country. Wells was only six when illness and 
+        financial troubles shattered his household, forcing his parents to send him to state-run Bavarian 
+        boarding schools. This early experience informs Wells’s überpersonal fourth novel, The End of Loneliness (Penguin Books),
+        an international phenomenon that won the European Union Prize for Literature. The tale centers on 
+        the orphaned Jules and his two older siblings, following the trio through a series of events that are 
+        unfortunate but also joyful and funny. It is not a perfect novel, but it is a life-affirming work.
+        As Jules says near the end, “I’m always surprised by how the light of memory makes particular moments 
+        shine brightly.” </h3>";
+    ?> 
+
+</body>
+</html>

ce63b4f18860bdfc90ebeb58e9acfc11fc79adfe
AminFitri <aminfitri2000@gmail.com>
2021-09-02 12:02:05 +0800

    pertama kali push

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Home Page</title>
+    <link rel="stylesheet" href="home.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+    <center>
+    <body>
+        <img src="books1.jpg" width="50%" height="200px"/>
+        <div class="topnav">
+        <a class="active" href="home.php">Home</a>
+        <a href="list.php">List Book</a>
+        <a href="form.php">Borrow Form</a>
+        </div>
+
+    <?php
+        echo "<h2>WELCOME TO ONLINE BOOK STORE !</h2>";
+    ?> 
+    </center>
+        <h3>INTRODUCTION :</h3>
+            <p><center>Since its fuse in 2020, ONLINE BOOK STORE has cut a specialty for itself in the youngsters’ books section of the distributing business. We have some expertise in books implied for offspring of various ages. The wide scope of books offered by us incorporates fantasies, showed story books, reference books, general learning books, sticker books and some more. Every one of these books are accessible in English.</center></p>
+
+</body>
+</html>
